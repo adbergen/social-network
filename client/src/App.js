76696c4efe640c615b0f9dcaa1e2cca9184e9222
Anthony Bergen <anthonydbergen@gmail.com>
2020-11-04 14:29:28 -0500

    added front end sign up and login pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,24 @@
-import React from "react";
-import NavBar from "./components/Nav/nav-bar";
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navbar from "./components//Navbar/Navbar";
+import Landing from "./components/Landing/Landing";
+import Register from "./components/Register/Register";
+import Login from "./components/Login/Login";
+
 import "./App.css";
 
-function App() {
-  return (
-    <header>
-      <div>
-        <NavBar />
-      </div>
-    </header>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+        </div>
+      </Router>
+    );
+  }
 }
-
 export default App;
