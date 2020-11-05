d6282dcef204130e0a8aa6a2c19c6fadd9681e30
Anthony Bergen <anthonydbergen@gmail.com>
2020-11-04 21:30:12 -0500

    updated theme in landing and dashboard page

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -6,16 +6,15 @@ class Navbar extends Component {
     return (
       <div className="navbar-fixed">
         <nav className="z-depth-0">
-          <div className="nav-wrapper white">
+          <div className="nav-wrapper black">
             <Link
-              to="/"
+              to="/dashboard"
               style={{
                 fontFamily: "monospace",
               }}
-              className="col s5 brand-logo center black-text"
+              className="col s5 brand-logo center white-text"
             >
-              <i className="material-icons">code</i>
-              MERN
+              Social Network
             </Link>
           </div>
         </nav>
