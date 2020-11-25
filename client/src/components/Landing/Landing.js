ce8eb5a5b3b50df8293bc6fc84666523f79048ce
Anthony Bergen <anthonydbergen@gmail.com>
2020-11-25 11:46:27 -0500

    converted Landing component from class to hook

--- client/src/components/Landing/Landing.js
+++ client/src/components/Landing/Landing.js
@@ -1,55 +1,50 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
-class Landing extends Component {
-  render() {
-    return (
-      <div
-        style={{ height: "75vh", backgroundColor: "rgba(255, 0, 0, 0.5)" }}
-        className="container valign-wrapper"
-      >
-        <div className="row">
-          <div className="col s12 center-align">
-            <h4>
-              <b>Meet</b> friends with the{" "}
-              <span style={{ fontFamily: "monospace" }}>Social Network</span>{" "}
-              app
-            </h4>
-            <p className="flow-text grey-text text-darken-1">
-              A True Social Experience
-            </p>
-            <br />
-            <div className="col s6">
-              <Link
-                to="/register"
-                style={{
-                  width: "140px",
-                  borderRadius: "3px",
-                  letterSpacing: "1.5px",
-                }}
-                className="btn btn-large waves-effect waves-light hoverable blue accent-3"
-              >
-                Register
-              </Link>
-            </div>
-            <div className="col s6">
-              <Link
-                to="/login"
-                style={{
-                  width: "140px",
-                  borderRadius: "3px",
-                  letterSpacing: "1.5px",
-                }}
-                className="btn btn-large waves-effect waves-light hoverable blue accent-3"
-              >
-                Log In
-              </Link>
-            </div>
+export default function Landing() {
+  return (
+    <div
+      style={{ height: "75vh", backgroundColor: "rgba(255, 0, 0, 0.5)" }}
+      className="container valign-wrapper"
+    >
+      <div className="row">
+        <div className="col s12 center-align">
+          <h4>
+            <b>Meet</b> friends with the{" "}
+            <span style={{ fontFamily: "monospace" }}>Social Network</span> app
+          </h4>
+          <p className="flow-text grey-text text-darken-1">
+            A True Social Experience
+          </p>
+          <br />
+          <div className="col s6">
+            <Link
+              to="/register"
+              style={{
+                width: "140px",
+                borderRadius: "3px",
+                letterSpacing: "1.5px",
+              }}
+              className="btn btn-large waves-effect waves-light hoverable blue accent-3"
+            >
+              Register
+            </Link>
+          </div>
+          <div className="col s6">
+            <Link
+              to="/login"
+              style={{
+                width: "140px",
+                borderRadius: "3px",
+                letterSpacing: "1.5px",
+              }}
+              className="btn btn-large waves-effect waves-light hoverable blue accent-3"
+            >
+              Log In
+            </Link>
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
-
-export default Landing;
