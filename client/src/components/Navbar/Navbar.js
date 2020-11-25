13e1f1933b4efbcd22ff306d56233091b590359b
Anthony Bergen <anthonydbergen@gmail.com>
2020-11-25 11:47:47 -0500

    converted Navbar component from class to hook

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,26 +1,22 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
-class Navbar extends Component {
-  render() {
-    return (
-      <div className="navbar-fixed">
-        <nav className="z-depth-0">
-          <div className="nav-wrapper black">
-            <Link
-              to="/dashboard"
-              style={{
-                fontFamily: "monospace",
-              }}
-              className="col s5 brand-logo center white-text"
-            >
-              Social Network
-            </Link>
-          </div>
-        </nav>
-      </div>
-    );
-  }
+export default function Navbar() {
+  return (
+    <div className="navbar-fixed">
+      <nav className="z-depth-0">
+        <div className="nav-wrapper black">
+          <Link
+            to="/dashboard"
+            style={{
+              fontFamily: "monospace",
+            }}
+            className="col s5 brand-logo center white-text"
+          >
+            Social Network
+          </Link>
+        </div>
+      </nav>
+    </div>
+  );
 }
-
-export default Navbar;
