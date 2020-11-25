9cb7e94f209883dd6cc6ce00bce3f23d6b4408d7
Anthony Bergen <anthonydbergen@gmail.com>
2020-11-25 12:44:16 -0500

    implemented dark mode theme via react context

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,37 @@
-import React from "react";
+import React, { createContext, useReducer } from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+import { initialState, reducer } from "./reducer";
 
-ReactDOM.render(
-  <App />,
+import Nav from "./Nav";
 
-  document.getElementById("root")
-);
+export const AppContext = createContext();
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { currentTheme } = state;
+
+  return (
+    <ThemeProvider theme={currentTheme}>
+      <AppContext.Provider value={{ ...state, dispatch }}>
+        <GlobalStyles />
+        <Nav />
+        <h1>Hi Dev.to!</h1>
+      </AppContext.Provider>
+    </ThemeProvider>
+  );
+}
+
+const GlobalStyles = createGlobalStyle`
+  html, body {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+    background: ${(props) => props.theme.backgroundColor};
+    color: ${(props) => props.theme.textColor};
+    font-family: sans-serif;
+  }
+`;
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
